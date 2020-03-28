c3af0cb0283d024210dd1b5cbc4e775a56d8a98e
Dmitrii <dshitikov@rtech.ru>
2020-03-28 18:34:43 +0300

    dmitrii-property-container

---
+++ tests/dmitrii/fundumental/ContainerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\dmitrii\fundumental;
+
+use PHPUnit\Framework\TestCase;
+use Src\dmitrii\fundumental\property_container\Post;
+
+class ContainerTest extends TestCase
+{
+    public function testContainer()
+    {
+        $post = new Post();
+
+        $this->assertNull($post->getProperty('title'));
+
+        $this->expectExceptionMessage('title not exist.');
+        $post->setProperty('title', 'Bye');
+
+        $post->addProperty('title', 'Hello!');
+        $this->assertEquals('Hello!', $post->getProperty('title'));
+
+        $post->setProperty('title', 'Bye');
+        $this->assertEquals('Bye', $post->getProperty('title'));
+
+        $post->deleteProperty('title');
+        $this->assertNull($post->getProperty('title'));
+    }
+}
